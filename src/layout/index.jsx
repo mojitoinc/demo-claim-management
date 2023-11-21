a1425629ee1ea17ef4a38e0a7e198dc0a0c64844
kameshkishoreIonixx <kameshkishore.s@ionixxtech.com>
2023-11-21 22:15:32 +0530

    v1.0.2-beta.10

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { ClaimTokenModal } from "@mojito-inc/claim-management";
+import { ClaimTokenModal, useWallet, useTransaction } from "@mojito-inc/claim-management";
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 // SDK claim modal integration
 
 const AppLayout = () => {
+  const { address, balance, networkDetails, provider, providerType } = useWallet();
+  console.log('provider', provider); // Web3 provider
+  const { transactionDetails, fetchInvoiceDetail, error } = useTransaction();
   const [openModal, setOpenModal] = useState(false);
-  const [wallet, setWallet] = useState();
   const onCloseModal = () => {
     setOpenModal(false);
   }
@@ -14,38 +17,26 @@ const AppLayout = () => {
     setOpenModal(true);
   }
 
-  const onGetWalletData = (
-      address,
-      networkDetails,
-      chainId,
-      balance,
-      provider,
-      providerType
-    ) => {
-      setWallet({
-        isDisconnect: false,
-        walletAddress: address,
-        networkDetails: networkDetails,
-        chainId: chainId,
-        balance: balance,
-        provider: provider,
-        providerType: providerType,
-      });
-    };
   return (
     <div>
       <Button onClick={ onOpenModal }>Open Modal</Button>
+      <Typography>{ address }</Typography>
+      <Typography>Native: { balance?.native } Non-native: { balance?.nonNative }</Typography>
+      <Typography>{ networkDetails?.chainID } { networkDetails?.name }</Typography>
+      <Typography>{ providerType }</Typography>
+      <Typography>{ transactionDetails?.invoiceId }</Typography>
+      <Typography>{ transactionDetails?.status }</Typography>
+      <Typography>{ transactionDetails?.transactionHash }</Typography>
       <ClaimTokenModal
         open={openModal} // If true it will open modal
         onCloseModal={onCloseModal} //function to modal close
-        onWalletData={onGetWalletData} // In Callback to get connected wallet details
-        walletAddress={wallet?.walletAddress} // Connected wallet address
+        name="Test" // User name
+        userEmail="tset@gmail.com" // User email
         config={{
-          CHAIN_ID: 1,
-          CROSSMINT_API: "sk_live.UlOd89oC.62pBPSMHUinkxpXsDY4tbT8dfvbbAZEE", // cross mint connection config
-          CROSSMINT_ENV: "STAGING",  // cross mint connection environment config
-          NETWORK_ID: "d608a5e7-8c80-4e7b-bb11-768ae855402a", // Network Id
-          ORG_ID: "8d95625a-5848-440c-8e55-0d008c9a1d38", //organization Id
+          chainId: 1,
+          crossmintApiKey: "sk_live.UlOd89oC.62pBPSMHUinkxpXsDY4tbT8dfvbbAZEE", // cross mint connection config
+          crossmintEnv: "STAGING",  // cross mint connection environment config
+          orgId: "8d95625a-5848-440c-8e55-0d008c9a1d38", //organization Id
           paperClientId: "659af8f3-6a4f-4f53-8936-ba0fa32b0db0", // Paper client Id for email wallet
           paperNetworkName: "Sepolia", // Paper network name
         }}
@@ -58,6 +49,13 @@ const AppLayout = () => {
         }}
         claimCode="" // pass claim code if isEnterClaimCode is false
         isEnterClaimCode={true} // if true user need to enter the claim code
+        link={{
+          logoUrl: '', // APP logo
+          termsUrl: '', // terms and conditions url
+          doNotHaveWalletURL: '', // do not have wallet button to redirect user 
+          viewTokenTrackerURL: '' // token tracker url
+        }}
+        onSuccess={ () => {} } // function to handle your own login in success modal button
       />
     </div>
   )
