bb476d6095c981244d83a1e28a6f64a9cda52378
kameshkishoreIonixx <kameshkishore.s@ionixxtech.com>
2023-12-05 14:03:09 +0530

    Next with typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React, {
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+  useMemo,
+} from "react";
+import { ClaimTokenModal, useWallet } from "@mojito-inc/claim-management";
+import router from "next/router";
+import moment from "moment";
+
+const Home = () => {
+  return (
+    <>
+      Test
+    </>
+  );
+};
+export default Home;
